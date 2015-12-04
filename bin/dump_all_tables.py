5468571e25ae9213f85109a78479034a99f698aa
linsalrob <raedwards@gmail.com>
2015-12-04 07:06:54 -0800

    dumping sqlite

---
+++ bin/dump_all_tables.py
@@ -0,0 +1,17 @@
+import sqlite3
+import pandas as pd
+
+
+def to_csv(filename):
+    db = sqlite3.connect(filename)
+    cursor = db.cursor()
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+    for table_name in tables:
+        table_name = table_name[0]
+        table = pd.read_sql_query("SELECT * from %s" % table_name, db)
+        table.to_csv(table_name + '.csv', index_label='index')
+
+
+if __name__ == '__main__':
+    parser=
