c1d4f249c9a09a15494b971570d0475aa9014d00
Robert Edwards <raedwards@gmail.com>
2015-12-08 11:52:00 +1100

    extracting

---
+++ bin/extract.py
@@ -0,0 +1,14 @@
+import os
+import subprocess
+import sys
+
+__author__ = 'Rob Edwards'
+
+print('CWD'  + os.getcwd())
+
+fname =  os.path.join(os.environ['HOME'], 'Dropbox/Metagenomics/51.hits_small.fa.lrz')
+
+f = subprocess.Popen(['/usr/bin/lrunzip', '-q', '-d', '-f', '-o -', fname], stdout=subprocess.PIPE).stdout
+
+for l in f:
+    print("READ: {}".format(l))
