f5f937cbe580c12a7231ae846de2ab62be1dce78
Robert Edwards <raedwards@gmail.com>
2016-03-04 14:30:36 -0800

    adding the raw data

---
+++ bin/resample.py
@@ -0,0 +1,12 @@
+"""
+
+"""
+
+import os
+import sys
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('', help='')
+    args = parser.parse_args()
