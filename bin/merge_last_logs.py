bd7210ac5cc95373ebb5c10822b5779cc8669226
Robert Edwards <raedwards@gmail.com>
2016-04-15 17:07:13 -0600

    sorting

--- bin/merge_last_logs.py
+++ bin/merge_last_logs.py
@@ -18,6 +18,18 @@ if __name__ == '__main__':
     # redwards         pts/0    anthill          Thu Feb 11 16:39:56 -0800 2016
     # redwards         pts/42   rohan.sdsu.edu   Wed Apr 13 17:14:03 -0700 2016
 
+
+    special_users = ['named', 'webalizer', 'nut', 'squid', 'sync', 'netdump', 'rpc', 'smmsp', 'shutdown', 'mysql',
+                     'operator', 'rpm', 'gdm', 'postgres', 'ftp', 'uucp', 'mailman', 'mailnull', 'distcache', 'pvm',
+                     'hsqldb', 'beaglidx', 'lp', 'ldap', 'mail', 'ganglia', 'adm', 'bin', 'prime', 'fax', 'nslcd',
+                     'halt', 'abrt', 'ntp', 'nobody', 'sshd', 'gopher', 'privoxy', 'pegasus', 'dovecot', 'apache',
+                     'news', 'xfs', 'pcap', 'haldaemon', 'biobike3', 'biobike2', 'daemon', 'ident', 'tomcat', 'rpcuser',
+                     'postfix', 'dbus', 'biobike4', 'ais', 'nfsnobody', 'games', 'vcsa', 'nscd', 'avahi', 'saslauth',
+                     'fig', 'jottotoo', 'root', 'jotto']
+    special_users.sort()
+    special_users_set = set(special_users)
+
+
     access = {}
     log = {}
     never = {}
@@ -25,8 +37,10 @@ if __name__ == '__main__':
     for f in args.l:
         with open(f, 'r') as fin:
             for l in fin:
+                p=l.strip().split()
+                if p[0] in special_users_set:
+                    continue
                 if 'Never logged in' in l:
-                    p = l.split()
                     never[p[0]] = l
                     continue
                 m = re.match('(\S+)\s+(\S+)\s+(\S+)\s+(.*?)$', l)
@@ -50,8 +64,14 @@ for k in log:
         never.pop(k)
 sys.stderr.write("{} never logged in\n".format(len(never.keys())))
 
+for s in special_users:
+    sys.stdout.write("{}: special user. Do not deactivate\n".format(s))
+sys.stdout.write("\n\nNEVER LOGGED IN\n")
+
 for n in never.values():
     sys.stdout.write(n)
+
+sys.stdout.write("\n\nLOGGED IN USERS\n")
 
 ke = sorted(log, key=access.__getitem__)
 for l in ke:
