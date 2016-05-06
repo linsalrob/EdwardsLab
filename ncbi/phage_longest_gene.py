a1d978dc91a4617ae6cb611bcc5818331e635d0b
Robert Edwards <raedwards@gmail.com>
2016-05-06 14:25:48 -0700

    Find the longest genes in some phages

--- ncbi/phage_longest_gene.py
+++ ncbi/phage_longest_gene.py
@@ -8,13 +8,25 @@ import os
 import sys
 
 import StringIO
+from Bio import SeqIO
 from ftplib import FTP
+
+
+def process_a_file(filename):
+    print("Phage file: " + f)
+    r = StringIO()
+    ftp.retrbinary('RETR genbank/' + f, r.write)
+    for seq in SeqIO.read(r.getvalue(), 'genbank'):
+        for feature in seq.features:
+            if 'locus_tag' in feature.qualifiers:
+                lt = feature.qualifiers['locus_tag'][0]
+            if 'translation' in feature.qualifiers:
+                print("{}\t{}".format(lt, len(feature.qualifiers['translation'][0])))
+
 
 # first find all the phg files in genbank
 
 ftp = FTP('ftp.ncbi.nlm.nih.gov')
 ftp.login()
 ftp.cwd('genbank/')
-for f in ftp.retrlines('LIST'):
-    if 'phg' in f:
-        print("Phage file: " + f)
+ftp.retrlines('NLST', process_a_file)
