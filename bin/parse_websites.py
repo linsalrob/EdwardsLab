3b9fb59661f441d974e8cbff64dc6bb17df940ed
Robert Edwards <raedwards@gmail.com>
2016-11-24 10:28:31 +1000

    Adding an error

--- bin/parse_websites.py
+++ bin/parse_websites.py
@@ -15,23 +15,27 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     for f in os.listdir(args.d):
-        aid = f.replace('.aspx', '')
-        soup = BeautifulSoup(open(os.path.join(args.d, f), 'r'), 'html.parser')
+        try:
+            aid = f.replace('.aspx', '')
+            soup = BeautifulSoup(open(os.path.join(args.d, f), 'r'), 'html.parser')
 
-        print("Organism\t{}".format(soup.title.get_text()))
-        print("ID\t{}".format(aid))
-        for table in soup.find_all('table', class_="fulllist"):
-            for row in table.find_all('tr'):
-                header = row.find('th').get_text()
-                cell = row.find('td').get_text()
+            print("Organism\t{}".format(soup.title.get_text()))
+            print("ID\t{}".format(aid))
+            for table in soup.find_all('table', class_="fulllist"):
+                for row in table.find_all('tr'):
+                    header = row.find('th').get_text()
+                    cell = row.find('td').get_text()
 
-                if header:
-                    header = header.strip()
-                    # header = header.encode('ascii', 'ignore')
+                    if header:
+                        header = header.strip()
+                        # header = header.encode('ascii', 'ignore')
 
-                if cell:
-                    cell = cell.strip()
-                    # cell = cell.encode('ascii', 'ignore')
+                    if cell:
+                        cell = cell.strip()
+                        # cell = cell.encode('ascii', 'ignore')
 
-                print("{}\t{}".format(header, cell))
-        print("//")
+                    print("{}\t{}".format(header, cell))
+            print("//")
+        except Exception as err:
+            sys.stderr.write("There was an error parsing {}\n{}\n. Skipped\n".format(f, err))
+
