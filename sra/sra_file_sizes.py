9de7c2585c32cc6515f96ed0071aa0695c2e585e
Robert Edwards <raedwards@gmail.com>
2016-11-25 11:22:09 +1000

    Updating to args

--- sra/sra_file_sizes.py
+++ sra/sra_file_sizes.py
@@ -10,15 +10,11 @@ import requests
 __author__ = 'Rob Edwards'
 
 if __name__ == "__main__":
-    """
-    parser = argparse.ArgumentParser(description=' ')
-    parser.add_argument('-f', help='input file', required=True)
+    parser = argparse.ArgumentParser(description='Retrieve the file sizes for the SRA data sets')
+    parser.add_argument('-f', help='file with tab separated list of SRA IDs and Metagenome Types (e.g. from PARTIE)', required=True)
     args = parser.parse_args()
-    """
 
-    args = "/home/redwards/Dropbox/GitHubs/partie/SRA_Metagenome_Types.tsv"
-
-    with open(args, 'r') as f:
+    with open(args.f, 'r') as f:
         for l in f:
             sraid, type = l.strip().split("\t")
             # According to: https://www.ncbi.nlm.nih.gov/books/NBK158899/#SRA_download.determining_the_location_of
