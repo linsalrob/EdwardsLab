76170410ffbd0496bcb4fd15ad6f0c1345edfa8e
Robert Edwards <raedwards@gmail.com>
2017-02-12 16:34:03 -0800

    Adding a hit counter

---
+++ bam/count_bam_hits.py
@@ -0,0 +1,41 @@
+"""
+Find runs that match crAssphage with a minimum number of runs, a minimum alignment length, and a minimum percent
+identity
+"""
+
+import os
+import sys
+import argparse
+
+import pysam
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Count hits in a bam file")
+    parser.add_argument('-d', help='Directory of bam files', required=True)
+    parser.add_argument('-l', help='Minimum alignment length (default = 30nt)', default=30, type=int)
+    parser.add_argument('-c', help='Minimum coverage as a fraction of read length (default = 0.9)', default=0.9, type=float)
+    parser.add_argument('-v', help='Verbose output', action='store_true')
+    args = parser.parse_args()
+
+    for f in os.listdir(args.d):
+        count = {}
+        found = False
+        if not f.endswith('.bam'):
+            if args.v:
+                sys.stderr.write("SKipped {}\n".format(f))
+            continue
+        bam = pysam.AlignmentFile(os.path.join(args.d, f), 'rb')
+
+        for p in bam.pileup(truncate=True):
+            for pilups in p.pileups:
+                if pilups.alignment.query_length < args.l:
+                    continue
+                alignment_frac = 1.0 * pilups.alignment.query_alignment_length / pilups.alignment.query_length
+                if alignment_frac <= args.c:
+                    continue
+                count[pilups.alignment.query_name] = count.get(pilups.alignment.query_name, 0) + 1
+                found = True
+
+        if found:
+            for q in count:
+                print("{}\t{}".format(q, count[q]))
