ad2241a83db8c1fe4a04bb9f24a63f8c6d45e507
Robert Edwards <raedwards@gmail.com>
2017-03-13 18:04:27 -0700

    parsing trees to rename and correct negative branch lengths

--- roblib/__init__.py
+++ roblib/__init__.py
@@ -8,11 +8,12 @@ from .dna import rc, shannon
 from .geography import latlon2distance
 from .strings import ascii_clean
 from .functions import is_hypothetical
+from .newick import Newick_Tree
 
 __all__ = [
     'mean', 'median', 'stdev',
     'read_fasta', 'readFasta', 'stream_fastq',
     'rc', 'shannon',
     'latlon2distance',
-    'ascii_clean', 'is_hypothetical'
+    'ascii_clean', 'is_hypothetical', 'Newick_Tree'
     ]
