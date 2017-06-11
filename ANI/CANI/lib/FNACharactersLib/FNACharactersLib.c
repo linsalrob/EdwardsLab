51c88f81a81460d01f968963d6f04a8267067af4
jbeacher6 <jon@jons-Mac-mini.local>
2017-06-10 18:29:46 -0700

    fnalib

--- ANI/CANI/lib/FNACharactersLib/FNACharactersLib.c
+++ ANI/CANI/lib/FNACharactersLib/FNACharactersLib.c
@@ -2,16 +2,18 @@
 #include <string.h>
 #include "FNACharactersLib.h"
 
-char fnaCharactersOf(const char* fnaFileNameAndLocation) {
+char* fnaCharactersOf(const char* fnaFileNameAndLocation) {
     FILE *fnaPointer;
-    char* fnaName = fnaFileNameAndLocation;
-    char str[100], char firstLineOfChars[100], dnaChars[8000000];
+    const char* fnaName = fnaFileNameAndLocation;
+    char singleCharLine[100], firstLineOfChars[100], dnaChars[8000000];
     fnaPointer = fopen(fnaName, "r");
-    if (fnaPointer == NULL){ return 1; }
+    //if (fnaPointer == NULL){ return 1; }
     fgets(firstLineOfChars, 100, fnaPointer);
     while (fgets(singleCharLine, 100, fnaPointer) != NULL) { 
-    	strcat(dnaChars, singleCharLine); 
+    	strcat(dnaChars, singleCharLine);
     }
     fclose(fnaPointer);
-    return dnaChars;
+    char* dnaCharsHeap = strdup(dnaChars);//or memcopy(sizeOf)
+    return dnaCharsHeap;
+    return 0;
 }
--- ANI/CANI/lib/FNACharactersLib/FNACharactersLib.h
+++ ANI/CANI/lib/FNACharactersLib/FNACharactersLib.h
@@ -1,6 +1,6 @@
 #ifndef _FNACHARACTERSLIB_H_
 #define _FNACHARACTERSLIB_H_
 
-extern char fnaCharactersOf(const char* fnaFileNameAndLocation);
+extern char* fnaCharactersOf(const char* fnaFileNameAndLocation);
 
 #endif
