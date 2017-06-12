76ed956c96c3b037e805ba0b97b64524b884c6f2
jbeacher6 <jon@jons-Mac-mini.local>
2017-06-11 20:39:45 -0700

    lib

--- ANI/CANI/lib/queryKmersLib/queryKmersLib.c
+++ ANI/CANI/lib/queryKmersLib/queryKmersLib.c
@@ -3,11 +3,12 @@
 #include <stdlib.h>
 #include "queryKmersLib.h"
 
-char *queryKmersIntoArray(const char* dnaChars, int kmerNum) {
-	char *kmersArray[strlen(dnaChars)]; //max kmers? possible segmentation fault if too high(3million?)
+char* queryKmersIntoArray(const char* dnaChars, int kmerNum) {
+	char *kmersArray[strlen(dnaChars)]; 
 	for(int i = 0; i < strlen(dnaChars) - kmerNum + 1; i++) {
 		kmersArray[i] = strndup(dnaChars + i, kmerNum);
-		printf("%s\n", kmersArray[i]);
 	}
-	return (char *) kmersArray;
+	printf("%s\n", kmersArray[i]);
+	return *kmersArray;
 }
+
