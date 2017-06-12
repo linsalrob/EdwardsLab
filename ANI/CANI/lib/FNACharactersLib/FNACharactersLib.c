0cb78f244789b661b31d44b3478fd2570ccad8f2
jbeacher6 <jon@jons-Mac-mini.local>
2017-06-11 20:38:27 -0700

    lib

--- ANI/CANI/lib/FNACharactersLib/FNACharactersLib.c
+++ ANI/CANI/lib/FNACharactersLib/FNACharactersLib.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 #include "FNACharactersLib.h"
 
 char* fnaCharactersOf(const char* fnaFileNameAndLocation) {
@@ -7,13 +8,18 @@ char* fnaCharactersOf(const char* fnaFileNameAndLocation) {
     const char* fnaName = fnaFileNameAndLocation;
     char singleCharLine[100], firstLineOfChars[100], dnaChars[8000000];
     fnaPointer = fopen(fnaName, "r");
-    //if (fnaPointer == NULL){ return 1; }
     fgets(firstLineOfChars, 100, fnaPointer);
-    while (fgets(singleCharLine, 100, fnaPointer) != NULL) { 
-    	strcat(dnaChars, singleCharLine);
+    char x[100];
+    while (fgets(singleCharLine, 100, fnaPointer) != NULL) {
+        for(int i = 0; i < strlen(singleCharLine); i++) {
+            if(isspace((char)singleCharLine[i]) != 1) {
+                strcat(dnaChars, &singleCharLine[i]);
+                //printf("%c\n", (char)singleCharLine[i]);
+            }
+            //printf("%s\n",dnaChars);
+        }
     }
     fclose(fnaPointer);
     char* dnaCharsHeap = strdup(dnaChars);//or memcopy(sizeOf)
     return dnaCharsHeap;
-    return 0;
 }
