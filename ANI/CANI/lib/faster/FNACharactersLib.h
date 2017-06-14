53ec03f1792e25a4a5e573790036dd0e587e5e29
jbeacher6 <jon@jons-Mac-mini.local>
2017-06-14 12:04:19 -0700

    lib

--- ANI/CANI/lib/faster/FNACharactersLib.h
+++
@@ -1,6 +0,0 @@
-#ifndef _FNACHARACTERSLIB_H_
-#define _FNACHARACTERSLIB_H_
-
-extern char** fnaCharactersOf(const char* fnaFileNameAndLocation);
-
-#endif
--- ANI/CANI/lib/faster/editDistanceLib.h
+++
@@ -1,6 +0,0 @@
-#ifndef _EDITDISTANCELIB_H_
-#define _EDITDISTANCELIB_H_
-
-extern int editDistance(const char* S, const char* T, int sLength, int tLength);
-
-#endif
