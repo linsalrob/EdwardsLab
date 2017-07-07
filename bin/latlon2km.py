577ac4908b0f5b6ebd0aa31e9efe66845f0738e5
Robert Edwards <raedwards@gmail.com>
2017-07-07 10:53:26 -0700

    Making a single file with no dependencies

--- bin/latlon2km.py
+++ bin/latlon2km.py
@@ -5,9 +5,64 @@ Convert a triple of [name, lat, lon] to a matrix of pairwise distances
 import os
 import sys
 import argparse
-from roblib import geography
 
 __author__ = 'Rob Edwards'
+
+
+
+def latlon2distance(lat1, long1, lat2, long2, miles=False):
+    """Convert two coordinates to distance.
+
+    This is an approximation since the earth is not spherical, but accuracy is <100m, especially for close points
+
+    This code was taken from http://www.johndcook.com/python_longitude_latitude.html
+
+    Latitude is measured in degrees north of the equator; southern locations have negative latitude.
+    Similarly, longitude is measured in degrees east of the Prime Meridian. A location 10deg west of
+    the Prime Meridian, for example, could be expressed as either 350deg  east or as -10deg east.
+
+    Arguments: lat1, long1; lat2, long2; miles is a boolean. If you want miles set it to true. Else set it to false
+
+    """
+
+    # Convert latitude and longitude to
+    # spherical coordinates in radians.
+    degrees_to_radians = math.pi / 180.0
+
+    # phi = 90 - latitude
+    phi1 = (90.0 - lat1) * degrees_to_radians
+    phi2 = (90.0 - lat2) * degrees_to_radians
+
+    # theta = longitude
+    theta1 = long1 * degrees_to_radians
+    theta2 = long2 * degrees_to_radians
+
+    # Compute spherical distance from spherical coordinates.
+
+    # For two locations in spherical coordinates
+    # (1, theta, phi) and (1, theta, phi)
+    # cosine( arc length ) =
+    #    sin phi sin phi' cos(theta-theta') + cos phi cos phi'
+    # distance = rho * arc length
+
+    cos = (math.sin(phi1) * math.sin(phi2) * math.cos(theta1 - theta2) + math.cos(phi1) * math.cos(phi2))
+    arc = math.acos(cos)
+
+    # Remember to multiply arc by the radius of the earth
+    # in your favorite set of units to get length.
+    #
+    # To convert to miles multiple arc by 3960
+    # To convert to kilometers multiply arc by 6373
+
+    if miles:
+        arc *= 3960
+    else:
+        arc *= 6373
+
+    return arc
+
+
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Convert a triple of [name, lat, lon] to a matrix of pairwise distances')
@@ -38,7 +93,7 @@ if __name__ == "__main__":
                 sys.stdout.write("\t0")
                 continue
             #sys.stdout.write("{}, {} :: {}, {}".format(loc[fn][0], loc[fn][1], loc[tn][0], loc[tn][1]))
-            dist = geography.latlon2distance(loc[fn][0], loc[fn][1], loc[tn][0], loc[tn][1])
+            dist = latlon2distance(loc[fn][0], loc[fn][1], loc[tn][0], loc[tn][1])
             sys.stdout.write("\t{}".format(dist))
             #sys.stdout.write("\n")
         sys.stdout.write("\n")
