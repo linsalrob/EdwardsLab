ae65b3bd905c3eed4cb014736291227905a1f481
Robert Edwards <raedwards@gmail.com>
2017-08-08 18:27:36 +1000

    adding parse dna dist

--- roblib/__init__.py
+++ roblib/__init__.py
@@ -9,11 +9,12 @@ from .geography import latlon2distance
 from .strings import ascii_clean
 from .functions import is_hypothetical
 from .newick import Newick_Tree
+from .dnadist import parse_dnadist
 
 __all__ = [
     'mean', 'median', 'stdev',
     'read_fasta', 'readFasta', 'stream_fastq',
     'rc', 'shannon',
     'latlon2distance',
-    'ascii_clean', 'is_hypothetical', 'Newick_Tree'
+    'ascii_clean', 'is_hypothetical', 'Newick_Tree', 'parse_dnadist'
     ]
