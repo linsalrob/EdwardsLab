ec2bcf75234b8d2995e519eab8190e4ed09d3993
Robert Edwards <raedwards@gmail.com>
2017-08-15 10:34:45 +1000

    Creating an init

---
+++ taxon/__init__.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+__author__ = 'Rob Edwards'
+from .taxon import read_taxa, read_nodes, extended_names, read_names, read_divisions, read_gi_tax_id, read_tax_id_gi
+
+__all__ = [
+    'read_taxa', 'read_nodes', 'extended_names', 'read_names', 'read_divisions', 'read_gi_tax_id', 'read_tax_id_gi'
+    ]
+
+
+
+
+
