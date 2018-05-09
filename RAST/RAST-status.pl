312c91e9a43901d1e9035cf7f33f435e7e981ced
linsalrob <raedwards@gmail.com>
2018-05-09 10:45:06 -0700

    adding command line username/password option

--- RAST/RAST-status.pl
+++ RAST/RAST-status.pl
@@ -5,27 +5,37 @@ use RASTserver;
 $ENV{SAS_SERVER}="SEED";
 use Term::ReadKey;
 
-print "Please enter your RAST username:  ";
-my $user = ReadLine(0);
-chomp $user;
+die "$0 [-u username] [-p password] <list of jobs>" unless (defined $ARGV[0]);
 
-print "Please enter your RAST password:  ";
-ReadMode 2;
-my $password = ReadLine(0);
-chomp $password;
-ReadMode 1;
-print "\n";
+my @ids;
+my $user; my $password;
+while (@ARGV) {
+	my $t=shift @ARGV;
+	if ($t eq "-u") {$user = shift @ARGV}
+	elsif ($t eq "-p") {$password = shift @ARGV}
+	else {push @ids, $t}
+}
+
+if (!$user) {
+	print "Please enter your RAST username:  ";
+	$user = ReadLine(0);
+	chomp $user;
+}
+
+if (!$password) {
+	print "Please enter your RAST password:  ";
+	ReadMode 2;
+	$password = ReadLine(0);
+	chomp $password;
+	ReadMode 1;
+	print "\n";
+}
 
 
 my $rast=new RASTserver($user, $password);
 unless (defined $rast) {die "Can't connect ot the rast server"}
 
-
-
-
-
-die "$0 <list of jobs>" unless (defined $ARGV[0]);
-my $stat = $rast->status_of_RAST_job({-job => \@ARGV});
+my $stat = $rast->status_of_RAST_job({-job => \@ids});
 
 foreach my $job (sort {$a <=> $b} keys %$stat) {
 	print join("\t", $job, $stat->{$job}->{'status'}), "\n";
