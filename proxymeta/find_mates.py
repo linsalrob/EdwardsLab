2fc3c23fc75ec3ad24902c0c88393433e65d2f0e
Robert Edwards <raedwards@gmail.com>
2018-06-26 16:12:41 +1000

    Allowing to print reads

--- proxymeta/find_mates.py
+++ proxymeta/find_mates.py
@@ -6,6 +6,7 @@ import os
 import sys
 import argparse
 import re
+from roblib import stream_fastq
 
 def find_mates(blastnf1, blastnf2):
     """
@@ -36,30 +37,55 @@ def odd_reads(reads):
     """
     Report reads that don't have a mate with a blast match
     :param reads: the dict of reads from the blast file
-    :return:
+    :return: two sets of reads that are missing, one from read 1 and one from read 2
     """
 
+    m1 = set()
+    m2 = set()
     for r in reads:
         if reads[r][0] and not reads[r][1]:
             sys.stderr.write("No read 2: {}\n".format(r))
+            m2.add("{}.2".format(r))
 
         if not reads[r][0] and reads[r][1]:
             sys.stderr.write("No read 1: {}\n".format(r))
+            m1.add("{}.1".format(r))
+
+    return m1, m2
 
 
+def print_reads(miss1, miss2, fq1, fq2):
+    """
+    Print the missing reads from the two fastq files
+    :param miss1: the set of reads missing from fq1
+    :param miss2: the set of reads missing from fq2
+    :param fq1: the first fastq file
+    :param fq2: the second fastq file
+    :return:
+    """
 
+    for sid, allid, seq, qual in stream_fastq(fq1):
+        if sid in miss1:
+            sys.stdout.write("@{}\n{}\n+\n{}\n".format(allid, seq, qual))
+
+    for sid, allid, seq, qual in stream_fastq(fq2):
+        if sid in miss2:
+            sys.stdout.write("@{}\n{}\n+\n{}\n".format(allid, seq, qual))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="")
-    parser.add_argument('-b', help='blast output files (use two -b)', nargs=2)
+    parser.add_argument('-b', help='blast output files. Please provide exactly two files', nargs=2)
+    parser.add_argument('-q', help='fastq files. Please provide exactly two files', nargs=2)
     parser.add_argument('-v', help='verbose output', action="store_true")
     args = parser.parse_args()
 
+    sys.stderr.write("Using blast files: {}\nUsing fastq files: {}\n".format(args.b, args.q))
 
     if len(args.b) != 2:
         sys.stderr.write("Please provide two, and exactly two, blast output files\n")
         sys.exit(-1)
 
     reads = find_mates(*args.b)
-    odd_reads(reads)
+    miss1, miss2 = odd_reads(reads)
+    print_reads(miss1, miss2, *args.q)
