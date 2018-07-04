321f7ad7bf9677027ec580c6d012f9b26d76ca7c
linsalrob <raedwards@gmail.com>
2018-07-04 14:45:26 -0700

    adding verbose output

--- blast/blast_to_network.py
+++ blast/blast_to_network.py
@@ -9,13 +9,14 @@ import sys
 import argparse
 import networkx as nx
 
-def load_blast(blastf, maxeval=None, minper=None, minlen=None):
+def load_blast(blastf, maxeval=None, minper=None, minlen=None, verbose=False):
     """
     Load a blast file into the network
     :param blastf: blast tab separated file to read
     :param maxeval: maximum E value to include in the network
     :param minper: minimum percent to include in the network
     :param minlen: minimum alignment length to include in the network
+    :param verbose: add more output
     :return: a networkx objext
     :return type: networkx
     """
@@ -24,6 +25,11 @@ def load_blast(blastf, maxeval=None, minper=None, minlen=None):
     with open(blastf, 'r') as f:
         for l in f:
             p=l.strip().split("\t")
+            if p[0] == p[1]:
+                continue
+            if p[1] in G and p[0] in G[p[1]]:
+                continue
+
             if maxeval and float(p[10]) > maxeval:
                 continue
             if minper and float(p[2]) < minper:
@@ -31,8 +37,11 @@ def load_blast(blastf, maxeval=None, minper=None, minlen=None):
             if minlen and float(p[3]) < minlen:
                 continue
 
+
             weight = (float(p[2])/100) * float(p[3])
-            G.add_edge(p[0], p[1], {'weight' : weight})
+            if verbose:
+                sys.stderr.write("Addding edge: {} -> {} : {}\n".format(p[0], p[1], weight))
+            G.add_edges_from([(p[0], p[1], {'weight' : weight})])
 
     return G
 
@@ -48,6 +57,23 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
 
-    net = load_blast(args.b, maxeval=args.e, minper=args.p, minlen=args.l)
+    net = load_blast(args.b, maxeval=args.e, minper=args.p, minlen=args.l, verbose=args.v)
 
-    print("Network:\n\tEdges: {}\n\tNodes: {}\n")
+    print("Network:\n\tEdges: {}\n\tNodes: {}\n".format(len(net.edges), len(net.nodes)))
+
+
+    for c in nx.connected_components(net):
+        print(c)
+
+    sys.exit()
+
+    seen = set()
+    for n in net.nodes:
+        if n in seen:
+            continue
+        thisa = set(net.adj[n])
+        thisa.add(n)
+        seen.update(thisa)
+        print("{}\n".format(thisa))
+
+
