9a9cdf4d68be9081ebc26d88f6f15660294de2b0
Robert Edwards <raedwards@gmail.com>
2018-08-09 15:13:49 -0700

    parsing blast files

--- roblib/__init__.py
+++ roblib/__init__.py
@@ -10,11 +10,13 @@ from .strings import ascii_clean
 from .functions import is_hypothetical
 from .newick import Newick_Tree
 from .dnadist import parse_dnadist
+from .blast import stream_blast_results
 
 __all__ = [
     'mean', 'median', 'stdev',
     'read_fasta', 'readFasta', 'stream_fastq', 'stream_fasta',
     'rc', 'shannon',
     'latlon2distance',
-    'ascii_clean', 'is_hypothetical', 'Newick_Tree', 'parse_dnadist'
+    'ascii_clean', 'is_hypothetical', 'Newick_Tree', 'parse_dnadist',
+    'stream_blast_results'
     ]
