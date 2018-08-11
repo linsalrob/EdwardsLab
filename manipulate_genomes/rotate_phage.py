14e15472c62b312613ae34f6085a07887525ad91
Robert Edwards <raedwards@gmail.com>
2018-08-11 13:06:46 -0700

    Adding genome rewrite

--- manipulate_genomes/rotate_phage.py
+++ manipulate_genomes/rotate_phage.py
@@ -1,11 +1,19 @@
 """
-Rotate a fasta sequence so that the start is just before the terminase
+Rotate a fasta sequence so that the start is just before the terminase.
+
+We need a blast hit to the terminase large subunit, but we don't necessarily need
+a hit to the terminase small subunit. These two are usually adjacent and on the same
+strand (in 2,127 out of 2,165 times!), and so we look upstream of the
+large subunit. If we have the small subunit and it is upstream of the
+large subunit, we will put the break before the small subunit. Otherwise
+we put the break before the large subunit.
 """
 
 import os
 import sys
 import argparse
 from roblib import stream_blast_results, rc, read_fasta
+from roblib import stream_fasta, rc
 
 def read_blast_file(bf, evalue, verbose=False):
     """
@@ -52,6 +60,7 @@ def find_gene(brs):
     :return:
     """
 
+    genes = {}
     contigs = brs.keys()
     if len(contigs) == 1:
         sys.stderr.write("There was only one contig with a match ({})\n".format(list(contigs)[0]))
@@ -65,26 +74,80 @@ def find_gene(brs):
                 continue
             if is_consecutive(brs[c][strand]):
                 sys.stderr.write("There is a consecutive match to {} {} {}\n".format(c, max(brs[c][strand]), min(brs[c][strand])))
+                if c not in genes:
+                    genes[c] = {strand : [min(brs[c][strand]), max(brs[c][strand])] }
+                else:
+                    sys.stderr.write("Warning: two hits on different strands for {}\n".format(c))
+                    genes[c][strand]  = [min(brs[c][strand]), max(brs[c][strand])]
             else:
                 sys.stderr.write("There are multiple discontinuous matches to {}. Try adjusting the evalue\n".format(c))
 
+    return genes
 
 
+def introduce_break(fastaf, lsgenes, ssgenes, numbp, verbose=False):
+    """
+    Introduce a break into the contigs upstream of the large subunit, but also ignore the small subunit
+    :param fastaf: fasta file to break
+    :param lsgenes: dict of ls genes
+    :param ssgenes: dict of ss genes
+    :param numbp: how far upstream to break the DNA
+    :param verbose: more output
+    :return:
+    """
+
+    for seqid, seq in stream_fasta(fastaf):
+        if seqid in lsgenes:
+            breakat = 0
+            dorc = False
+            if '+ve' in lsgenes[seqid]:
+                # the terminase is on the + strand so we go n bp less than the mininmum
+                breakat = lsgenes[seqid]['+ve'][0] - numbp
+                # check to see if the small subunit is here too
+                if seqid in ssgenes and '+ve' in ssgenes[seqid]:
+                    if ssgenes[seqid]['+ve'][0] - numbp < breakat:
+                        breakat = ssgenes[seqid]['+ve'][0] - numbp
+                        if verbose:
+                            sys.stderr.write("Because we have a small subunit at {} we moved the start\n".format(ssgenes[seqid]['+ve'][0]))
+            elif '-ve' in lsgenes[seqid]:
+                dorc = True
+                # the terminase is on the - strand so we go n bp more than the maximum
+                breakat = lsgenes[seqid]['-ve'][1] + numbp
+                # check to see if the small subunit is here too
+                if seqid in ssgenes and '-ve' in ssgenes[seqid]:
+                    if ssgenes[seqid]['-ve'][1] + numbp > breakat:
+                        breakat = ssgenes[seqid]['-ve'][1] + numbp
+                        if verbose:
+                            sys.stderr.write("Because we have a small subunit at {} we moved the start\n".format(ssgenes[seqid]['+ve'][0]))
+            else:
+                sys.stderr.write("No terminase found in {}\n".format(seqid))
+
+            newseq = seq
+            if breakat > 0:
+                newseq = seq[breakat:] + seq[0:breakat]
+                if dorc:
+                    newseq = rc(newseq)
+            print(">{}\n{}".format(seqid, newseq))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Recircularize the genome at the terminase")
     parser.add_argument('-f', help='fasta file of the genome', required=True)
     parser.add_argument('-l', help='blast output compared to terminase LARGE gene', required=True)
-    parser.add_argument('-s', help='blast output compared to terminase SMALL gene', required=True)
+    parser.add_argument('-s', help='blast output compared to terminase SMALL gene')
     parser.add_argument('-e', help='evalue cutoff (default=all)', type=int, default=100)
+    parser.add_argument('-n', help='number of bp from the end of the terminase to introduce the break (default=100)', type=int, default=100)
     parser.add_argument('-v', help='verbose output', action="store_true")
     args = parser.parse_args()
 
-    sys.stderr.write("Parsing small subunit\n")
-    ss = read_blast_file(args.s, args.e, args.v)
-    find_gene(ss)
+    ssgenes = {}
+    if args.s:
+        sys.stderr.write("Parsing small subunit\n")
+        ss = read_blast_file(args.s, args.e, args.v)
+        ssgenes = find_gene(ss)
 
     sys.stderr.write("Parsing large subunit\n")
     ls = read_blast_file(args.l, args.e, args.v)
-    find_gene(ls)
+    lsgenes = find_gene(ls)
+
+    introduce_break(args.f, lsgenes, ssgenes, args.n, args.v)
