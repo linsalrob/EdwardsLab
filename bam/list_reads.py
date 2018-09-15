a0d4fbda1c6dadef5922905dcb89b805cde4b3aa
linsalrob <raedwards@gmail.com>
2018-09-15 14:53:38 -0700

    bamfile

--- bam/list_reads.py
+++ bam/list_reads.py
@@ -12,6 +12,6 @@ parser.add_argument('-b', help='bam file', required=True)
 parser.add_argument('-v', help='verbose output', action="store_true")
 args = parser.parse_args()
 
-bamfile = pysam.AlignmentFile(bamf, "rb")
+bamfile = pysam.AlignmentFile(args.b, "rb")
 for read in bamfile.fetch(until_eof=True):
     print(read.query_name)
