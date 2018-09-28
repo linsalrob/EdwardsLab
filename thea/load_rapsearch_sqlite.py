7e724813b751740aa7cc4413f376446c2bf218de
Robert Edwards <raedwards@gmail.com>
2018-09-28 11:54:38 -0700

    sqlite

--- thea/load_rapsearch_sqlite.py
+++ thea/load_rapsearch_sqlite.py
@@ -45,56 +45,43 @@ def disconnect(conn, verbose=False):
         sys.stderr.write("There was no database connection!\n")
 
 
-def create_table(genomeid, conn, verbose=False):
+def create_table(conn, verbose=False):
     """
     Create a table for a given genomeid
-    :param genomeid: the genome id
     :param conn: the database connection
     :param verbose: more output
     :return: the database connection
     """
 
-    if verbose:
-        sys.stderr.write("Creating a table for {}\n".format(genomeid))
-
-
     rows =  'query TEXT PRIMARY KEY, subject TEXT, identity REAL, len INTEGER, mismatch INTEGER,'
     rows += ' gap_open INTEGER, query_start INTEGER, query_end INTEGER, subject_start INTEGER,'
     rows += ' subject_end INTEGER, e_value REAL, bitscore REAL'
 
-
-
-
-    cl = "CREATE TABLE {tn} ({rows})".format(tn=genomeid, rows=rows)
+    cl = "CREATE TABLE rapsearch ({rows})".format(rows=rows)
     if verbose:
         sys.stderr.write("{}\n".format(cl))
 
     conn.execute(cl)
-    conn.execute("CREATE INDEX {tn}QIndex on {tn}(query)".format(tn=genomeid))
-    conn.execute("CREATE INDEX {tn}SIndex on {tn}(subject)".format(tn=genomeid))
+    conn.execute("CREATE INDEX rapsearchQIndex on rapsearch(query)")
+    conn.execute("CREATE INDEX rapsearchSIndex on rapsearch(subject)")
 
     conn.commit()
 
     return conn
 
-def load_data(genomeid, filename, conn, verbose=False):
+def load_data(filename, conn, verbose=False):
     """
     Load the data fromt the m8 format file
-    :param genomeid: the genome id to load
     :param filename: the file for the m8 format data
     :param conn: the database connection
     :param verbose: more output
     :return:
     """
 
-    if verbose:
-        sys.stderr.write("Adding data for {}\n".formnat(genomeid))
-
     if filename.endswith('.gz'):
         qin = gzip.open(filename, 'rt')
     else:
         qin = open(filename, 'r')
-
 
     for l in qin:
         if l.startswith('#'):
@@ -106,8 +93,9 @@ def load_data(genomeid, filename, conn, verbose=False):
         for i in [3, 4, 5, 6, 7, 8, 9]:
             p[i] = int(p[i])
         """
-        c = "INSERT INTO {tn} (query subject identity len mismatch gap_open query_start query_end subject_start subject_end e_value bitscore)".format(tn=genomeid)
-        c += "values ({val})".format(val=" ".join(p))
+        c = "INSERT INTO rapsearch (query, subject, identity, len, mismatch, gap_open, query_start, query_end, subject_start, subject_end, e_value, bitscore)".format(tn=genomeid)
+        c += "values ('{val}')".format(val="', '".join(p))
+
         try:
             conn.execute(c)
         except sqlite3.OperationalError as e:
@@ -126,8 +114,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     conn = createdb(args.s, args.v)
+    create_table(conn, args.v)
+
     for f in os.listdir(args.d):
-        genid = f.replace('.m8.gz', '')
-        genid = genid.replace('kate.', '')
-        create_table(genid, conn, args.v)
-        load_data(genid, os.path.join(args.d, f), conn, args.v)
+        load_data(os.path.join(args.d, f), conn, args.v)
