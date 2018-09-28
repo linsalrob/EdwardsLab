156161233c3c2ce1333d258d637e2402155031b2
Robert Edwards <raedwards@gmail.com>
2018-09-28 08:07:27 -0700

    additional information

--- thea/locate_orfs.py
+++ thea/locate_orfs.py
@@ -9,60 +9,79 @@ from roblib import translate_dna, stream_fasta, rc
 
 __author__ = 'Rob Edwards'
 
+
+def read_sources(sf):
+    """
+    Read the sources.txt file that has source of ORF call, ORF id
+    :param sf: sources.txt file
+    :return: dict of ORF/source
+    """
+
+    s = {}
+    with open(sf, 'r') as f:
+        for l in f:
+            p = l.strip().split("\t")
+            s[p[0]] = p[1]
+    return s
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Print the locations of the ORFs on the sequence')
     parser.add_argument('-f', help='fasta DNA sequence file', required=True)
     parser.add_argument('-o', help='ORFs file', required=True)
+    parser.add_argument('-s', help='sources file that has the source of the ORF calls (default = sources.txt)', default="sources.txt")
     parser.add_argument('-v', help='verbose output', action='store_true')
     args = parser.parse_args()
+
+    sources = read_sources(args.s)
 
     # generate the six frame translations
     seqs = {}
     lengths = {}
     for seqid, seq in stream_fasta(args.f):
         seq = seq.upper()
-        # this is a total hack to avoid having to recreate the original seqid
-        lengths["{}_f1".format(seqid)] = lengths["{}_f2".format(seqid)] = lengths["{}_f3".format(seqid)] = len(seq)
-        lengths["{}_r1".format(seqid)] = lengths["{}_r2".format(seqid)] = lengths["{}_r3".format(seqid)] = len(seq)
-        seqs["{}_f1".format(seqid)] = translate_dna(seq, args.v)
-        seqs["{}_f2".format(seqid)] = translate_dna(seq[1:], args.v)
-        seqs["{}_f3".format(seqid)] = translate_dna(seq[2:], args.v)
+        lengths[seqid] = len(seq)
+        seqs[seqid]["f1"] = translate_dna(seq, args.v)
+        seqs[seqid]["f2"] = translate_dna(seq[1:], args.v)
+        seqs[seqid]["f3"] = translate_dna(seq[2:], args.v)
         rcseq = rc(seq)
-        seqs["{}_r1".format(seqid)] = translate_dna(rcseq, args.v)
-        seqs["{}_r2".format(seqid)] = translate_dna(rcseq[1:], args.v)
-        seqs["{}_r3".format(seqid)] = translate_dna(rcseq[2:], args.v)
+        seqs[seqid]["r1"] = translate_dna(rcseq, args.v)
+        seqs[seqid]["r2"] = translate_dna(rcseq[1:], args.v)
+        seqs[seqid]["r3"] = translate_dna(rcseq[2:], args.v)
 
     for orfid, orf in stream_fasta(args.o):
         for s in seqs:
-            if orf in seqs[s]:
-                start = seqs[s].index(orf) * 3 # convert from aa to bp!
-                frame = 0
-                # shit, now we have to figure out frames!
-                if s.endswith('_f1'):
-                    # first frame, this is easy
-                    start += 1 # index is 0 indexed!
-                    frame = 1
-                    end = start + (3 * len(orf)) - 1 # if the aa string includes * this will too.
-                elif s.endswith('_f2'):
-                    start += 2
-                    frame = 2
-                    end = start + (3 * len(orf)) - 1
-                elif s.endswith('_f3'):
-                    start += 3
-                    frame = 3
-                    end = start + (3 * len(orf)) - 1
-                elif s.endswith('_r1'):
-                    start = (lengths[s] - start)
-                    frame = -1
-                    end = start - (3 * len(orf)) + 1
-                elif s.endswith('_r2'):
-                    start = (lengths[s] - (start+1))
-                    frame = -2
-                    end = start - (3 * len(orf)) + 1
-                elif s.endswith('_r3'):
-                    start = (lengths[s] - (start+2))
-                    frame = -3
-                    end = start - (3 * len(orf)) + 1
+            for fr in ["f1", "f2", "f3", "r1", "r2", "r3"]:
+                if orf in seqs[s][fr]:
+                    start = seqs[s][fr].index(orf) * 3 # convert from aa to bp!
+                    frame = 0
+                    end = 0
+                    # shit, now we have to figure out frames!
+                    if 'f1' == fr:
+                        # first frame, this is easy
+                        start += 1 # index is 0 indexed!
+                        frame = 1
+                        end = start + (3 * len(orf)) - 1 # if the aa string includes * this will too.
+                    elif 'f2' == fr:
+                        start += 2
+                        frame = 2
+                        end = start + (3 * len(orf)) - 1
+                    elif 'f3' == fr:
+                        start += 3
+                        frame = 3
+                        end = start + (3 * len(orf)) - 1
+                    elif 'r1' == fr:
+                        start = (lengths[s] - start)
+                        frame = -1
+                        end = start - (3 * len(orf)) + 1
+                    elif 'r2' == fr:
+                        start = (lengths[s] - (start+1))
+                        frame = -2
+                        end = start - (3 * len(orf)) + 1
+                    elif 'r3' == fr:
+                        start = (lengths[s] - (start+2))
+                        frame = -3
+                        end = start - (3 * len(orf)) + 1
 
-                print("\t".join(map(str, [orfid, start, end, frame])))
-                break
+
+                    print("\t".join(map(str, [sources.get(orfid, 'UNKNOWN'), orfid, s, start, end, frame, lengths[s]])))
+                    break
