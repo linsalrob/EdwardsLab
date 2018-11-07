f4f58cbbe9907d3630b1f65b3db5e0f2f2bc795e
Robert Edwards <raedwards@gmail.com>
2018-11-08 08:56:17 +1030

    Only one read per genome

--- thea/count_lastal_hits.cpp
+++ thea/count_lastal_hits.cpp
@@ -3,6 +3,7 @@
 // Process every file in the lastal directory and make a count of all the hits
 #include <boost/filesystem.hpp>
 #include <boost/range/iterator_range.hpp>
+#include <boost/algorithm/string.hpp>
 #include <boost/iostreams/filtering_streambuf.hpp>
 #include <boost/iostreams/copy.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
@@ -12,6 +13,7 @@
 #include <cstring>
 #include <sstream>
 #include <map>
+#include <unordered_set>
 #include <zlib.h>
 using namespace std;
 
@@ -26,6 +28,9 @@ int main (int argc, char* argv[]) {
         cerr << "Please provide a directory with all the files to summarize" << endl;
         return 1;
     }
+
+// FILTER THIS SO THAT WE DON'T COUNT THE SAME GENOME TWICE PER READ
+// filter with 1e-5 evalue
 
     cout << "Filename\tpeg\tcounts\ttotal per mg\tnormalized" << endl;
     map<string, float> counts;
@@ -43,12 +48,32 @@ int main (int argc, char* argv[]) {
 
         string line;
         map<string, float> counts;
+        map<string, unordered_set<string>> seen;
         int total;
         while (getline(instream, line)) {
             if ('#' != line[0]) {
                 istringstream ss(line);
-                string peg; string match;
-                ss >> match >> peg;
+                // Fields: query id, subject id, % identity, alignment length, mismatches, gap opens, q. start, q. end, s. start, s. end, evalue, bit score, query length, subject length
+                string peg; string seq_read; float per_ident; int aln_len; int m_m; int open_s; int q_start; int q_end; int s_start; int s_end;
+                float e_val; int bit_score; int q_len; int s_len;
+                ss >> seq_read >> peg >> per_ident >> aln_len >> m_m >> open_s >> q_start >> q_end >> s_start >> s_end >> e_val >> bit_score >> q_len >> s_len;
+
+                // ignore everything with an e value above 1e-5
+                if (1e-5 > e_val)
+                    continue;
+
+                // split the genome.orf to genome and orf
+                vector<string> parts;
+                boost::split(parts, peg, [](char c){return '.' == c;});
+
+                // check to see if we have seen this read mapping to this genome before. If so, ignore it
+                // If not, add the genome to the list per read.
+                if (seen.count(seq_read) > 0 && seen[seq_read].count(parts[0]) > 0)
+                    continue;
+                if (0 == seen.count(seq_read))
+                    seen[seq_read] = unordered_set<string>();
+                seen[seq_read].insert(parts[0]);
+
                 counts[peg]++;
                 total++;
             }
