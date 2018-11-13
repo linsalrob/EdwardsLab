a112574d8c1f18b15ed8c22a4c1d2e3b8968a7e4
Robert Edwards <raedwards@gmail.com>
2018-11-13 12:13:31 +0800

    adding gi_taxid

--- taxon/sqlite_taxon.py
+++ taxon/sqlite_taxon.py
@@ -9,6 +9,7 @@ import sqlite3
 import json
 
 from config import get_db_dir
+import gzip
 
 defaultdir = get_db_dir()
 
@@ -47,184 +48,29 @@ def disconnect(conn, verbose=False):
     elif verbose:
         sys.stderr.write("There was no database connection!\n")
 
-def define_tables(conn, verbose=False):
-    """
-    Define the database
-    :param conn: the database connection
-    :param verbose: print addtional output
-    :return: the database connection
-    """
-    """
-    Tables in the database (note that the tables have the same name as the files on purpose!):
-    
-    Note: You can find more information about these files at ftp://ftp.ncbi.nih.gov/pub/taxonomy/taxdump_readme.txt
-    
-    nodes:
-            tax_id                                  -- node id in GenBank taxonomy database
-            parent tax_id                           -- parent node id in GenBank taxonomy database
-            rank                                    -- rank of this node (superkingdom, kingdom, ...)
-            embl code                               -- locus-name prefix; not unique
-            division id                             -- see division.dmp file
-            inherited div flag  (1 or 0)            -- 1 if node inherits division from parent
-            genetic code id                         -- see gencode.dmp file
-            inherited GC  flag  (1 or 0)            -- 1 if node inherits genetic code from parent
-            mitochondrial genetic code id           -- see gencode.dmp file
-            inherited MGC flag  (1 or 0)            -- 1 if node inherits mitochondrial gencode from parent
-            GenBank hidden flag (1 or 0)            -- 1 if name is suppressed in GenBank entry lineage
-            hidden subtree root flag (1 or 0)       -- 1 if this subtree has no sequence data yet
-            comments                                -- free-text comments and citations
-        
-    
-    names:
-            tax_id                                  -- the id of node associated with this name
-            name_txt                                -- name itself
-            unique name                             -- the unique variant of this name if name not unique
-            name class                              -- (synonym, common name, ...)
-    
-    division:
-            division id                             -- taxonomy database division id
-            division cde                            -- GenBank division code (three characters)
-            division name                           -- e.g. BCT, PLN, VRT, MAM, PRI...
-            comments
-    
-    gencode:
-            genetic code id                         -- GenBank genetic code id
-	        abbreviation                            -- genetic code name abbreviation
-	        name                                    -- genetic code name
-	        cde                                     -- translation table for this genetic code
-	        starts                                  -- start codons for this genetic code
-        
-    """
-
-
-    tables = {
-        "nodes" : {
-            "tax_id"                     : "INTEGER",
-            "parent"                     : "INTEGER",
-            "rank"                       : "TEXT",
-            "embl_code"                  : "TEXT",
-            "division_id"                : "INTEGER",
-            "inherited_div"              : "INTEGER",
-            "genetic_code"               : "INTEGER",
-            "inherited_genetic_code"     : "INTEGER",
-            "mitochondrial_genetic_code" : "INTEGER",
-            "inherited_mito_gc"          : "INTEGER",
-            "genbank_hidden"             : "INTEGER",
-            "hidden_subtree"             : "INTEGER",
-            "comments" : ""
-        },
-        "names" : {
-            "tax_id"                     : "INTEGER",
-            "name"                       : "TEXT",
-            "unique_name"                : "TEXT",
-            "name_class"                 : "TEXT"
-        },
-        "division" : {
-            "division_id"                : "INTEGER",
-            "division_code"              : "TEXT",
-            "division_name"              : "TEXT",
-            "comments"                   : ""
-        },
-        "gencode" : {
-            "genetic_code"               : "INTEGER",
-            "abbreviation"               : "",
-            "name"                       : "TEXT",
-            "cde"                        : "TEXT",
-            "starts"                     : "TEXT"
-        }
-    }
-
-    # note that tax_id is not unique in names
-    primary_keys = {"nodes" : "tax_id", "division" : "division_id", "gencode" : "genetic_code", "names" : ""}
-    for t in tables:
-        createl = []
-        for c in tables[t]:
-            if primary_keys[t] == c:
-                createl.append("{} {} PRIMARY KEY".format(c, tables[t][c]))
-            else:
-                createl.append("{} {}".format(c, tables[t][c]))
-        if verbose:
-            sys.stderr.write("Going to create table with command: ")
-            sys.stderr.write("CREATE TABLE {tn} ({rows})".format(tn=t, rows=", ".join(createl)))
-            sys.stderr.write("\n")
-        conn.execute("CREATE TABLE {tn} ({rows})".format(tn=t, rows=", ".join(createl)))
-    conn.commit()
-
-    return conn
-
-
-def load_tables(conn, datadir, verbose=False):
-    """
-    Parse the files and load the tables
-    :param conn: the database connection
-    :param datadir:  the data directory with the files
-    :param verbose: print addtional output
-    :return: the database connection
-    """
-
-    tables = {
-        "nodesXX": ["tax_id", "parent", "rank", "embl_code", "division_id", "inherited_div", "genetic_code",
-                      "inherited_genetic_code", "mitochondrial_genetic_code", "inherited_mito_gc", "genbank_hidden",
-                      "hidden_subtree", "comments"],
-        "names": ["tax_id", "name", "unique_name", "name_class"],
-        "division": ["division_id", "division_code", "division_name", "comments"],
-        "gencode": ["genetic_code", "abbreviation", "name", "cde", "starts"]
-    }
-
-
-    for t in tables:
-        if not os.path.exists(os.path.join(datadir, "{}.dmp".format(t))):
-            sys.stderr.write("ERROR: {}.dmp not found in {}\n".format(t, datadir))
-            continue
-        vcount = 0
-        with open(os.path.join(datadir, "{}.dmp".format(t)), 'r') as f:
-            for l in f:
-                pori = l.strip().rstrip("|").split('\t|')
-                p=[]
-                for i,j in enumerate(pori):
-                    x=j.replace('"', '')
-                    x=x.strip()
-                    p.append(x)
-                sys.stderr.write("{}\n".format(p))
-                # marvel in this json.dumps!
-                maxvcount  = 100
-                if verbose:
-                    vcount += 1
-                    if vcount < maxvcount:
-                        sys.stderr.write("INSERT INTO {tn} ({cn}) VALUES ({val})\n".format(
-                        tn=t, cn=", ".join(tables[t]), val=json.dumps(p).replace('[', '', 1).replace(']', '', 1)))
-                    if vcount == maxvcount:
-                        sys.stderr.write("Only first {mv} inputs printed\n\n".format(mv=maxvcount))
-                try:
-                    conn.execute("INSERT INTO {tn} ({cn}) VALUES ({val})".format(
-                        tn=t, cn=", ".join(tables[t]), val=json.dumps(p).replace('[', '', 1).replace(']', '', 1)
-                    ))
-                except sqlite3.OperationalError as e:
-                    sys.stderr.write("{}".format(e))
-                    sys.stderr.write("\nWhile performing:\n")
-                    sys.stderr.write("INSERT INTO {tn} ({cn}) VALUES ({val})\n".format(
-                        tn=t, cn=", ".join(tables[t]), val=json.dumps(p).replace('[', '', 1).replace(']', '', 1)))
-                    sys.exit()
-
-        conn.commit()
-    return conn
-
-
 def create_load(conn, datadir, verbose=False):
     """
-    Create the databases and load the data. This is not generic like the
-    other methods, but it allows for quotes etc in the names. grr.
+    Create the databases and load the data.
+
+    Note: Initially I wrote a generic version of this, but you need to have the correct
+    number of ?'s for this. We probably could generalize some of this, especially those
+    with two columns, but this works well.
+
     :param conn: the database connection
     :param datadir: the databae directory
     """
 
 
     ## The NODES table
+    dbfile = os.path.join(datadir, "nodes.dmp.gz")
     if verbose:
-        sys.stderr.write("loading nodes table\n")
+        sys.stderr.write("loading NODES table: {}\n".format(dbfile))
+    if not os.path.exists(dbfile):
+        sys.stderr.write("ERROR: {} does not exist\n".format(dbfile))
+        sys.exit(-1)
     conn.execute("CREATE TABLE nodes (tax_id INTEGER PRIMARY KEY, parent INTEGER, rank TEXT, embl_code TEXT, division_id INTEGER, inherited_div INTEGER, genetic_code INTEGER, inherited_genetic_code INTEGER, mitochondrial_genetic_code INTEGER, inherited_mito_gc INTEGER, genbank_hidden INTEGER, hidden_subtree INTEGER, comments)")
     conn.commit()
-    with open(os.path.join(datadir, "nodes.dmp"), "r") as f:
+    with gzip.open(dbfile, "rt") as f:
         for l in f:
             p = l.strip().rstrip("|").split('\t|')
             p = [x.strip() for x in p]
@@ -238,11 +84,15 @@ def create_load(conn, datadir, verbose=False):
 
 
     # the NAMES table
+    dbfile = os.path.join(datadir, "names.dmp.gz")
     if verbose:
-        sys.stderr.write("loading names table\n")
+        sys.stderr.write("loading NAMES table: {}\n".format(dbfile))
+    if not os.path.exists(dbfile):
+        sys.stderr.write("ERROR: {} does not exist\n".format(dbfile))
+        sys.exit(-1)
     conn.execute("CREATE TABLE names (tax_id INTEGER, name TEXT, unique_name TEXT, name_class TEXT)")
     conn.commit()
-    with open(os.path.join(datadir, "names.dmp"), "r") as f:
+    with gzip.open(dbfile, "rt") as f:
         for l in f:
             p = l.strip().rstrip("|").split('\t|')
             p = [x.strip() for x in p]
@@ -255,11 +105,15 @@ def create_load(conn, datadir, verbose=False):
     conn.commit()
 
     # the DIVISION table
+    dbfile = os.path.join(datadir, "division.dmp.gz")
     if verbose:
-        sys.stderr.write("loading division table\n")
+        sys.stderr.write("loading DIVISION table: {}\n".format(dbfile))
+    if not os.path.exists(dbfile):
+        sys.stderr.write("ERROR: {} does not exist\n".format(dbfile))
+        sys.exit(-1)
     conn.execute("CREATE TABLE division (division_id INTEGER PRIMARY KEY, division_code TEXT, division_name TEXT, comments)")
     conn.commit()
-    with open(os.path.join(datadir, "division.dmp"), "r") as f:
+    with gzip.open(dbfile, "rt") as f:
         for l in f:
             p = l.strip().rstrip("|").split('\t|')
             p = [x.strip() for x in p]
@@ -272,11 +126,15 @@ def create_load(conn, datadir, verbose=False):
     conn.commit()
 
     # the GENETIC CODE table
+    dbfile = os.path.join(datadir, "gencode.dmp.gz")
     if verbose:
-        sys.stderr.write("loading gencode table\n")
+        sys.stderr.write("loading GENETIC CODE table: {}\n".format(dbfile))
+    if not os.path.exists(dbfile):
+        sys.stderr.write("ERROR: {} does not exist\n".format(dbfile))
+        sys.exit(-1)
     conn.execute("CREATE TABLE gencode (genetic_code INTEGER, abbreviation , name TEXT, cde TEXT, starts TEXT)")
     conn.commit()
-    with open(os.path.join(datadir, "gencode.dmp"), "r") as f:
+    with gzip.open(dbfile, "rt") as f:
         for l in f:
             p = l.strip().rstrip("|").split('\t|')
             p = [x.strip() for x in p]
@@ -289,11 +147,15 @@ def create_load(conn, datadir, verbose=False):
     conn.commit()
 
     # the MERGED database
+    dbfile = os.path.join(datadir, "merged.dmp.gz")
     if verbose:
-        sys.stderr.write("loading merged table\n")
+        sys.stderr.write("loading MERGED table: {}\n".format(dbfile))
+    if not os.path.exists(dbfile):
+        sys.stderr.write("ERROR: {} does not exist\n".format(dbfile))
+        sys.exit(-1)
     conn.execute("CREATE TABLE merged (old_tax_id INTEGER, new_tax_id INTEGER)")
     conn.commit()
-    with open(os.path.join(datadir, "merged.dmp"), "r") as f:
+    with gzip.open(dbfile, "rt") as f:
         for l in f:
             p = l.strip().rstrip("|").split('\t|')
             p = [x.strip() for x in p]
@@ -304,6 +166,49 @@ def create_load(conn, datadir, verbose=False):
                 sys.stderr.write("\nWhile insert on: {}\n".format(p))
                 sys.exit()
     conn.commit()
+
+    # the gi_taxid
+    dbfile = os.path.join(datadir, "gi_taxid_nucl.dmp.gz")
+    if verbose:
+        sys.stderr.write("loading GI_TAXID (NUCL) table: {}\n".format(dbfile))
+    if not os.path.exists(dbfile):
+        sys.stderr.write("ERROR: {} does not exist\n".format(dbfile))
+        sys.exit(-1)
+    conn.execute("CREATE TABLE gi_taxid_nucl (gi INTEGER PRIMARY KEY, tax_id INTEGER)")
+    conn.commit()
+    with gzip.open(dbfile, "rt") as f:
+        for l in f:
+            p = l.strip().rstrip("|").split('\t|')
+            p = [x.strip() for x in p]
+            try:
+                conn.execute("INSERT INTO merged VALUES (?, ?)", p)
+            except sqlite3.OperationalError as e:
+                sys.stderr.write("{}".format(e))
+                sys.stderr.write("\nWhile insert on: {}\n".format(p))
+                sys.exit()
+    conn.commit()
+
+    # the gi_taxid
+    dbfile = os.path.join(datadir, "gi_taxid_prot.dmp.gz")
+    if verbose:
+        sys.stderr.write("loading GI_TAXID (PROT) table: {}\n".format(dbfile))
+    if not os.path.exists(dbfile):
+        sys.stderr.write("ERROR: {} does not exist\n".format(dbfile))
+        sys.exit(-1)
+    conn.execute("CREATE TABLE gi_taxid_prot (gi INTEGER PRIMARY KEY, tax_id INTEGER)")
+    conn.commit()
+    with gzip.open(dbfile, "rt") as f:
+        for l in f:
+            p = l.strip().rstrip("|").split('\t|')
+            p = [x.strip() for x in p]
+            try:
+                conn.execute("INSERT INTO merged VALUES (?, ?)", p)
+            except sqlite3.OperationalError as e:
+                sys.stderr.write("{}".format(e))
+                sys.stderr.write("\nWhile insert on: {}\n".format(p))
+                sys.exit()
+    conn.commit()
+
 
     return conn
 
@@ -353,8 +258,6 @@ if __name__ == '__main__':
             sys.exit(-1)
 
     conn = connect_to_db(args.s, args.v)
-    # conn = define_tables(conn, args.v)
-    # conn = load_tables(conn, args.d, args.v)
     conn = create_load(conn, args.d, args.v)
     conn = create_indices(conn, args.v)
     disconnect(conn, args.v)
