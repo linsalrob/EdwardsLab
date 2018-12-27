e206aa4ddeb6659e97f1f2743aa1ba05aed47e5a
linsalrob <raedwards@gmail.com>
2018-12-27 15:07:07 -0800

    add colors to python

---
+++ roblib/bcolors.py
@@ -0,0 +1,28 @@
+"""
+Colors that you can import and make the text look pretty
+
+Source: https://stackoverflow.com/questions/287871/print-in-terminal-with-colors
+"""
+
+__author__ = 'Rob Edwards'
+
+
+class bcolors(object):
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+    PINK = '\033[95m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    WHITE = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
