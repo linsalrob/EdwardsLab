0dc2456b87244c55c5ddf9dca0e70e8d8c93d244
linsalrob <raedwards@gmail.com>
2019-11-25 15:49:11 -0800

    Calculate shannon entropy and evenness of kmers in a fasta file

--- kmers/kmer_entropy_sa.py
+++ kmers/kmer_entropy_sa.py
@@ -28,6 +28,38 @@ def rc(dna):
     rcseq = dna.translate(complements)[::-1]
     return rcseq
 
+
+
+def stream_fastq(fqfile):
+    """Read a fastq file and provide an iterable of the sequence ID, the
+    full header, the sequence, and the quaity scores.
+
+    Note that the sequence ID is the header up until the first space,
+    while the header is the whole header.
+    """
+
+    if fqfile.endswith('.gz'):
+        qin = gzip.open(fqfile, 'rt')
+    else:
+        qin = open(fqfile, 'r')
+
+    while True:
+        header = qin.readline()
+        if not header:
+            break
+        header = header.strip()
+        seqidparts = header.split(' ')
+        seqid = seqidparts[0]
+        seqid = seqid.replace('@', '')
+        seq = qin.readline()
+        seq = seq.strip()
+        qualheader = qin.readline()
+        qualscores = qin.readline().strip()
+        header = header.replace('@', '', 1)
+        yield seqid, header, seq, qualscores
+
+
+
 def stream_fasta(fastafile,):
     """
     Stream a fasta file, one read at a time. Saves memory!
@@ -75,6 +107,35 @@ def stream_fasta(fastafile,):
         yield idline, seq
 
 
+def count_kmers_fastq(faf, k, verbose=False):
+    """
+    Count the kmers
+    :param faf: fasta file
+    :param k: kmer size
+    :param verbose: more output
+    :return: a dict of kmers
+    """
+    if verbose:
+        sys.stderr.write(f"{GREEN}Counting kmers (k={k}) in {faf}{ENDC}\n")
+
+    if not os.path.exists(faf):
+        sys.stderr.write(f"{RED}FATAL: {faf} does not exist\n{ENDC}")
+        sys.exit(2)
+
+    kmers = {}
+    for id, header, seq, qual in stream_fastq(faf):
+        rcseq = rc(seq)
+        posn = 0
+        while posn < len(seq) - k - 1:
+            kmers[seq[posn:posn+k]] = kmers.get(seq[posn:posn+k], 0) + 1
+            kmers[rcseq[posn:posn + k]] = kmers.get(rcseq[posn:posn + k], 0) + 1
+            posn += 1
+
+    if verbose:
+        sys.stderr.write(f"{BLUE}\tDone counting kmers (k={k}) in {faf}{ENDC}\n")
+
+    return kmers
+
 def count_kmers(faf, k, verbose=False):
     """
     Count the kmers
@@ -87,8 +148,12 @@ def count_kmers(faf, k, verbose=False):
     if verbose:
         sys.stderr.write(f"{GREEN}Counting kmers (k={k}) in {faf}{ENDC}\n")
 
-    kmers = {}
+    if not os.path.exists(faf):
+        sys.stderr.write(f"{RED}FATAL: {faf} does not exist\n{ENDC}")
+        sys.exit(2)
 
+
+    kmers = {}
     for id, seq in stream_fasta(faf):
         rcseq = rc(seq)
         posn = 0
@@ -131,20 +196,28 @@ def evenness(kmers, H=None, verbose=False):
     if not H:
         H = shannon(kmers, verbose)
     S = len(kmers.keys())
-    return H/log2(S)
+    return H/log2(S), log2(S)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Count the kmers in a file and report entropy and eveness')
-    parser.add_argument('-f', help='fasta file to count the entropy/evenness', required=True)
+    parser.add_argument('-f', help='fasta file to count the entropy/evenness')
+    parser.add_argument('-q', help='fastq file to count the entropy/eveness')
     parser.add_argument('-k', help='kmer size', required=True, type=int)
     parser.add_argument('-t', help='print field titles in output', action='store_true')
     parser.add_argument('-v', help='verbose output', action='store_true')
     args = parser.parse_args()
 
-    kmers = count_kmers(args.f, args.k, args.v)
+    if args.q:
+        kmers = count_kmers_fastq(args.q, args.k, args.v)
+    elif args.f:
+        kmers = count_kmers(args.f, args.k, args.v)
+    else:
+        sys.stderr.write(f"{RED}FATAL: Either -f (fasta) or -q (fastq) must be specified\n{ENDC}")
+        sys.exit(2)
+
     H = shannon(kmers, args.v)
-    e = evenness(kmers, H, args.v)
+    e,s = evenness(kmers, H, args.v)
 
     if args.t:
         print("File\tK-mer size\tShannon's Entropy\tEvenness")
-    print(f"{args.f}\t{args.k}\t{H}\t{e}")
+    print(f"{args.f}\t{args.k}\t{H}\t{s}\t{e}")
