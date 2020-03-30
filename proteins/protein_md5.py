2c7852d383c74c8d82dc6da2fd20d315a80b655e
Robert Edwards <raedwards@gmail.com>
2020-03-30 11:12:28 -0700

    Adding a minimum length, and ignoring proteins with stop codons.

--- proteins/protein_md5.py
+++ proteins/protein_md5.py
@@ -8,10 +8,11 @@ import argparse
 import hashlib
 from roblib import stream_fasta, bcolors
 
-def read_fasta(fafile, idmapfile, verbose=False):
+def read_fasta(fafile, idmapfile, minlen=300, verbose=False):
     """
     Read a fasta file and return a dict of proteins and their md5 sums
     :param fafile: fasta file
+    :param minlen: minimum protein sequence length (in amino acids) to be included
     :param verbose: more output
     :return:
     """
@@ -22,6 +23,11 @@ def read_fasta(fafile, idmapfile, verbose=False):
     seqs = {}
     with open(idmapfile, 'a') as out:
         for seqid, seq in stream_fasta(fafile):
+            # ignore a sequence with a stop codon
+            if '*' in seq:
+                continue
+            if len(seq) < minlen:
+                continue
             md5 = hashlib.md5(seq.upper().encode('utf-8')).hexdigest()
             seqs[md5] = seq
             out.write(f"{md5}\t{seqid}\n")
@@ -37,17 +43,18 @@ if __name__ == '__main__':
     parser.add_argument('-f', help='file')
     parser.add_argument('-d', help='directory of fasta files')
     parser.add_argument('-i', help='id map file to write')
+    parser.add_argument('-m', help='minimum length of protein sequence to include (in amino acids). Default = 300', type=int, default=300)
     parser.add_argument('-o', help='output file')
     parser.add_argument('-v', help='verbose output', action='store_true')
     args = parser.parse_args()
 
     seqs = {}
     if args.f:
-        seqs.update(read_fasta(args.f, args.i, args.v))
+        seqs.update(read_fasta(args.f, args.i, args.m, args.v))
 
     if args.d:
         for f in os.listdir(args.d):
-            seqs.update(read_fasta(os.path.join(args.d, f), args.i, args.v))
+            seqs.update(read_fasta(os.path.join(args.d, f), args.i, args.m, args.v))
 
     with open(args.o, 'w') as out:
         for m in seqs:
