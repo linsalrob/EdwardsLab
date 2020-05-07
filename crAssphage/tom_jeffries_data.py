6c272a4525b8119bca8b61df616e43f0cc3c7d75
Robert Edwards <raedwards@gmail.com>
2020-05-07 16:47:13 -0700

    solving for paired end - 3 file

--- crAssphage/tom_jeffries_data.py
+++ crAssphage/tom_jeffries_data.py
@@ -76,7 +76,7 @@ def focus_counts(data_directory, verbose=False):
                         continue
                     l = l.strip()
                     tax = ":".join(l.split(",")[0:lastcol])
-                    if ',0' in tax:
+                    if ':0' in tax:
                         message(f"Error parsing {sample} when lastcol was {lastcol}", "RED")
                         message(f"{l}", "BLUE")
                         message(f"{tax}", "PINK")
