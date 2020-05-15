fda0e2068d52b1ee9031e35d6df356e293c88c9d
linsalrob <raedwards@gmail.com>
2020-05-15 16:22:01 -0700

    handling multiple fast[aq] files at once

--- bin/countfasta.py
+++ bin/countfasta.py
@@ -14,46 +14,48 @@ __author__ = 'Rob Edwards'
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=' ')
-    parser.add_argument('-f', help='fasta file', required=True)
+    parser.add_argument('-f', nargs='+', help='fasta file', required=True)
     parser.add_argument('-l', help='list the lengths for each sequence (default = not to)', action='store_true')
     parser.add_argument('-m', help='minimum length fo be inclued', type=int, default=0)
     parser.add_argument('-t', help='tab separated output. Fields: [# seqs, total bp, shortest, longest, N50, N75]', action='store_true')
     args = parser.parse_args()
 
-    fa=read_fasta(args.f)
 
-    if len(fa.keys()) == 1 and list(fa.keys())[0] == '':
-        sys.stderr.write(f"No sequences found in {args.f}\n")
-        sys.exit(0)
+    for faf in args.f:
+        fa=read_fasta(faf)
 
-    if args.l:
-        for i in fa:
-            print("{}\t{}".format(i, len(fa[i])))
-        print()
+        if len(fa.keys()) == 1 and list(fa.keys())[0] == '':
+            sys.stderr.write(f"No sequences found in {faf}\n")
+            sys.exit(0)
 
-    lensall=[len(fa[i]) for i in fa]
-    lens = list(filter(lambda x: x > args.m, lensall))
-    lens.sort()
-    length=sum(lens)
+        if args.l:
+            for i in fa:
+                print("{}\t{}".format(i, len(fa[i])))
+            print()
 
-    len_so_far = 0
-    n50 = None
-    n75 = None
-    auN = 0
-    for i in lens:
-        len_so_far += i
-        if not n50 and len_so_far >= length * 0.5:
-            n50 = i
-        if not n75 and len_so_far >= length * 0.75:
-            n75 = i
-        auN += i**2
+        lensall=[len(fa[i]) for i in fa]
+        lens = list(filter(lambda x: x > args.m, lensall))
+        lens.sort()
+        length=sum(lens)
 
-    auN /= length
+        len_so_far = 0
+        n50 = None
+        n75 = None
+        auN = 0
+        for i in lens:
+            len_so_far += i
+            if not n50 and len_so_far >= length * 0.5:
+                n50 = i
+            if not n75 and len_so_far >= length * 0.75:
+                n75 = i
+            auN += i**2
 
-    if args.t:
-        print("\t".join(map(str, [args.f, len(lens), length, lens[0], lens[-1], n50, n75, auN])))
-    else:
-        print("Number of sequences: {}\nTotal length: {}\nShortest: {}\nLongest: {}\nN50: {}\nN75: {}\nauN: {}".format(
-            len(lens), length, lens[0], lens[-1], n50, n75, auN
-        ))
+        auN /= length
+
+        if args.t:
+            print("\t".join(map(str, [faf, len(lens), length, lens[0], lens[-1], n50, n75, auN])))
+        else:
+            print("File name: {}\nNumber of sequences: {}\nTotal length: {}\nShortest: {}\nLongest: {}\nN50: {}\nN75: {}\nauN: {}".format(
+                faf, len(lens), length, lens[0], lens[-1], n50, n75, auN
+            ))
 
--- bin/countfastq.py
+++ bin/countfastq.py
@@ -14,35 +14,36 @@ __author__ = 'Rob Edwards'
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=' ')
-    parser.add_argument('-f', help='fasta file', required=True)
+    parser.add_argument('-f', nargs='+', help='fasta file', required=True)
     parser.add_argument('-t', help='tab separated summmary of name, total len, shortest, longest, n50, n75', action="store_true")
     parser.add_argument('-s', help='(deprecated). Same as -t', action="store_true")
     args = parser.parse_args()
 
-    if not os.path.exists(args.f):
-        sys.stderr.write(f"{bcolors.RED}FATAL: {args.f} not found{bcolors.ENDC}\n")
-        sys.exit(1)
+    for faf in args.f:
+        if not os.path.exists(faf):
+            sys.stderr.write(f"{bcolors.RED}FATAL: {faf} not found{bcolors.ENDC}\n")
+            sys.exit(1)
 
-    lens = []
-    for (sid, label, seq, qual) in stream_fastq(args.f):
-        lens.append(len(seq))
-    lens.sort()
-    length=sum(lens)
+        lens = []
+        for (sid, label, seq, qual) in stream_fastq(faf):
+            lens.append(len(seq))
+        lens.sort()
+        length=sum(lens)
 
-    len_so_far = 0
-    n50 = None
-    n75 = None
+        len_so_far = 0
+        n50 = None
+        n75 = None
 
-    for i in lens:
-        len_so_far += i
-        if not n50 and len_so_far >= length * 0.5:
-            n50 = i
-        if not n75 and len_so_far >= length * 0.75:
-            n75 = i
+        for i in lens:
+            len_so_far += i
+            if not n50 and len_so_far >= length * 0.5:
+                n50 = i
+            if not n75 and len_so_far >= length * 0.75:
+                n75 = i
 
-    if args.s or args.t:
-        print("{}\t{}\t{}\t{}\t{}\t{}\t{}".format(args.f, len(lens), length, lens[0], lens[-1], n50, n75))
-    else:
-        print("Number of sequences: {}\nTotal length: {}\nShortest: {}\nLongest: {}\nN50: {}\nN75: {}".format(
-            len(lens), length, lens[0], lens[-1], n50, n75,
-        ))
+        if args.s or args.t:
+            print("{}\t{}\t{}\t{}\t{}\t{}\t{}".format(faf, len(lens), length, lens[0], lens[-1], n50, n75))
+        else:
+            print("File: {}\nNumber of sequences: {}\nTotal length: {}\nShortest: {}\nLongest: {}\nN50: {}\nN75: {}".format(
+                faf, len(lens), length, lens[0], lens[-1], n50, n75,
+            ))
