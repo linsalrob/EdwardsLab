cba1972494ff2fa16a6e3a8a580affc3fbe36aed
Robert Edwards <raedwards@gmail.com>
2020-07-03 17:13:01 -0700

    adding a function to convert m8 output to LCA taxonomy

--- taxon/__init__.py
+++ taxon/__init__.py
@@ -4,7 +4,7 @@ import sys
 __author__ = 'Rob Edwards'
 from .taxon import read_taxa, read_nodes, extended_names, read_names, read_divisions, read_gi_tax_id, read_tax_id_gi
 from .config import get_db_dir
-from .load_from_database import get_taxonomy_db, get_taxonomy, connect_to_db, get_taxid_for_name, taxonomic_hierarchy_as_list
+from .load_from_database import get_taxonomy_db, get_taxonomy, connect_to_db, get_taxid_for_name, taxonomy_hierarchy_as_list
 from .load_from_database import all_ids, taxonomy_hierarchy, all_species_ids
 from .taxonomy import TaxonNode, TaxonName, TaxonDivision
 from .Error import NoNameFoundError, EntryNotInDatabaseError
