a828f6acf5c5a008187e4a0c327272c4f569158b
Robert Edwards <raedwards@gmail.com>
2020-07-04 11:18:11 -0700

    Adding taxonomy

--- superfocus_all/superfocus_to_taxonomy.py
+++ superfocus_all/superfocus_to_taxonomy.py
@@ -111,7 +111,7 @@ def parse_m8_tophit(m8f, evalue, verbose=False):
                 if taxonomy[tid]:
                     r = "\t".join(taxonomy[tid])
                     printed.add(p[0])
-                    print(f"{p[0]}\t{r}")
+                    print(f"{p[0]}\t{r}\ttaxid: {tid}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=" ")
