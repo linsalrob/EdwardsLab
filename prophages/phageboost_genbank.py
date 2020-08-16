9bfebc35de632f44dc53590e03ab9e795c8847bf
Robert Edwards <raedwards@gmail.com>
2020-08-16 14:20:25 +0930

    providing model as option

--- prophages/phageboost_genbank.py
+++ prophages/phageboost_genbank.py
@@ -7,7 +7,7 @@ This is extracted from their example code, but we added parsing genbank files to
 
 import os
 import sys
-from roblib import genbank_to_pandas
+from roblib import genbank_to_pandas, message
 from PhageBoost.main import read_sequence_file, call_genes, calculate_features, read_model_from_file, predict, get_predictions
 import xgboost as xgb
 import argparse
@@ -19,11 +19,12 @@ __license__ = 'MIT'
 __maintainer__ = 'Rob Edwards'
 __email__ = 'raedwards@gmail.com'
 
-def run_phage_boost(genecalls, verbose):
+def run_phage_boost(genecalls, model_file, verbose):
     """
     Run phage boost
-    :param genecalls:
-    :param verbose:
+    :param model_file: The model file that is probably something like model_delta_std_hacked.pickled.silent.gz
+    :param genecalls: The pandas data frame of gene calls
+    :param verbose: more output
     :return:
     """
     # rolling params
@@ -38,15 +39,16 @@ def run_phage_boost(genecalls, verbose):
     neighbouring = 0
     alpha = 0.001
 
-    # files
-    model_file = '/home/redwards/pb/PhageBoost/PhageBoost/models/model_delta_std_hacked.pickled.silent.gz'
-
     # calculate features from gene calls
+    if verbose:
+        message("Calculating features", "GREEN")
     df = calculate_features(genecalls)
     # load model
     model, feats, feats_, limit = read_model_from_file(model_file)
     # transform data
     df = get_predictions.get_deltas(df[feats_])
+    if verbose:
+        message("Transforming gene predictions to regions", "GREEN")
     # transform single gene predictions to regions
     newgenecalls, nphages, res = predict(model, genecalls, df,
                                          feats, period, win_type,
@@ -58,15 +60,16 @@ def run_phage_boost(genecalls, verbose):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=" ")
-    parser.add_argument('-g', help='GenBank file to parse', required=True)
-    parser.add_argument('-o', help='output file for phage regions')
-    parser.add_argument('-v', help='verbose output', action='store_true')
+    parser.add_argument('-g', '--genbankfile', help='GenBank file to parse', required=True)
+    parser.add_argument('-m', '--modelfile', help="Model file. Probably something like model_delta_std_hacked.pickled.silent.gz", required=True)
+    parser.add_argument('-o', '--outputfile', help='output file for phage regions')
+    parser.add_argument('-v', '--verbose', help='verbose output', action='store_true')
     args = parser.parse_args()
 
-    genecalls = genbank_to_pandas(args.g, args.v)
-    res = run_phage_boost(genecalls, args.v)
-    if args.o:
-        with open(args.o, 'w') as out:
+    genecalls = genbank_to_pandas(args.genbankfile, args.verbose)
+    res = run_phage_boost(genecalls, args.modelfile, args.verbose)
+    if args.outputfile:
+        with open(args.outputfile, 'w') as out:
             res.to_csv(out, sep="\t", header=True)
     else:
         print(res)
