187c59b7d2a83e5500deccb56ad58157f2920d59
Rob Edwards <raedwards@gmail.com>
2020-12-01 17:07:22 +1030

    Plotting blast results

--- blast/simple_blast_plot.py
+++ blast/simple_blast_plot.py
@@ -16,7 +16,7 @@ __maintainer__ = 'Rob Edwards'
 __email__ = 'raedwards@gmail.com'
 
 
-def run(filename, outputfile, seqname, reflen, query=False,  verbose=False):
+def run(filename, outputfile, seqname, reflen, mergey, query=False,  verbose=False):
     fig = plt.figure()
     ax = fig.add_subplot(111)
 
@@ -57,7 +57,8 @@ def run(filename, outputfile, seqname, reflen, query=False,  verbose=False):
     yposn += 10
 
 
-
+    yposns = {}
+    lastyposn = yposn
     with open(filename, 'r') as f:
         for l in f:
             p = l.strip().split("\t")
@@ -74,16 +75,28 @@ def run(filename, outputfile, seqname, reflen, query=False,  verbose=False):
                 (start, stop) = (stop, start)
             start -= minx
             stop -= minx
+
+            if mergey:
+                yidx = p[0]
+                if query:
+                    yidx = p[1]
+                if yidx not in yposns:
+                    yposns[yidx] = lastyposn
+                    lastyposn += 1
+                yposn = yposns[yidx]
+
             ax.plot([start, stop], [yposn, yposn], color='blue')
             yposn += 1
 
     ax.set_xlabel(f"Position in {seqname} from {minx} to {maxx}")
     ax.set_ylabel("Individual sequences")
 
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
     ax.spines['top'].set_visible(False)
     ax.spines['right'].set_visible(False)
-    ax.get_xaxis().tick_bottom()
-    ax.get_yaxis().tick_left()
+    #ax.get_xaxis().tick_bottom()
+    #ax.get_yaxis().tick_left()
 
     fig.set_facecolor('white')
 
@@ -98,9 +111,10 @@ if __name__ == '__main__':
     parser.add_argument('-i', help='image file to write', required=True)
     parser.add_argument('-q', help='use the query locations. The default is to use the reference locations', action='store_true')
     parser.add_argument('-n', help='name of the reference (query with -q) sequence to use. The default is to ignore this and assume you only have one sequence', default=None)
+    parser.add_argument('-m', help='merge all hits with the same query (reference with -q) into a single line', action='store_true')
     parser.add_argument('-l', help='length of the reference (query with -q) sequence', default='auto')
 
     parser.add_argument('-v', help='verbose output', action='store_true')
     args = parser.parse_args()
 
-    run(args.f, args.i, args.n, args.l, args.q, args.v)
+    run(args.f, args.i, args.n, args.l, args.m, args.q, args.v)
