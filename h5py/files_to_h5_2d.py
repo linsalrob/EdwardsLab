81bf32ace22c3fe179c3e69fe2996ca15576689e
Rob Edwards <raedwards@gmail.com>
2021-10-18 16:46:34 +1030

    Reading and writing h5 files as 2d arrays

---
+++ h5py/files_to_h5_2d.py
@@ -0,0 +1,58 @@
+"""
+Convert a series of files to 2D h5 format.
+
+Here we have a series of files
+
+f1.tsv, f2.tsv, f3.tsv
+
+where each has
+
+[contig1, value]
+[contig2, value]
+[contig3, value]
+
+and we want to create a single 2D h5 object where we have rows are contigs and columns are samples
+
+
+"""
+
+import os
+import sys
+import argparse
+import h5py
+import numpy as np
+
+__author__ = 'Rob Edwards'
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Convert a series of data to h5py format')
+    parser.add_argument('-f', '--files', help='input files', nargs='+')
+    parser.add_argument('-o', '--output', help='output h5 file to write', required=True)
+    parser.add_argument('-v', '--verbose', help='verbose output', action='store_true')
+    parser.add_argument('-s', '--skip', help='skip lines that start *. This is very specific to parsed bam files',
+                        action='store_true')
+    args = parser.parse_args()
+
+    data = {}
+    all_contigs = set()
+    for fname in args.files:
+        with open(fname, 'r') as f:
+            data[fname] = {}
+            for l in f:
+                if args.skip and l.startswith('*'):
+                    continue
+                p = l.strip().split("\t")
+                data[fname][p[0]] = int(p[1])
+                all_contigs.add(p[0])
+
+    sorted_contigs = sorted(all_contigs)
+    sorted_files = sorted(args.files)
+
+    twod = []
+    for contig in sorted_contigs:
+        d = [data[f][contig] if contig in data[f] else 0 for f in sorted_files]
+        twod.append(d)
+
+    with h5py.File(args.output, "w") as f:
+        grp = f.create_dataset("contigs", data=np.array(twod))
+
