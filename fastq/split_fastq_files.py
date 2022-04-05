0c16af85b3a44f483507dc3ae66425364951e3e7
linsalrob <raedwards@gmail.com>
2022-04-05 16:01:08 +0930

    adding some errors

--- fastq/split_fastq_files.py
+++ fastq/split_fastq_files.py
@@ -27,7 +27,7 @@ if __name__ == "__main__":
     reads = {}
     filecounter = 1
     counter = 0
-    out = open(os.path.join(args.d, f"{args.o}_{filecounter}.R1.fastq"), 'w')
+    out = open(os.path.join(args.d, f"{args.o}_{filecounter}_R1.fastq"), 'w')
     if args.v:
         message(f"Writing left reads to {filecounter}", "GREEN")
     for sid, seqid, seq, qual in stream_fastq(args.l):
@@ -36,16 +36,17 @@ if __name__ == "__main__":
             counter = 0
             filecounter += 1
             out.close()
-            out = open(os.path.join(args.d, f"{args.o}_{filecounter}.R1.fastq"), 'w')
+            out = open(os.path.join(args.d, f"{args.o}_{filecounter}_R1.fastq"), 'w')
             if args.v:
                 message(f"Writing left reads to {filecounter}", "GREEN")
         out.write(f"@{seqid}\n{seq}\n+\n{qual}\n")
-        reads[sid] = filecounter
+        tsid = sid.replace('/1', '')
+        reads[tsid] = filecounter
     out.close()
 
     filecounter = 1
     counter = 0
-    out = open(os.path.join(args.d, f"{args.o}_{filecounter}.R2.fastq"), 'w')
+    out = open(os.path.join(args.d, f"{args.o}_{filecounter}_R2.fastq"), 'w')
     if args.v:
         message(f"Writing right reads to {filecounter}", "GREEN")
     for sid, seqid, seq, qual in stream_fastq(args.r):
@@ -54,15 +55,15 @@ if __name__ == "__main__":
             counter = 0
             filecounter += 1
             out.close()
-            out = open(os.path.join(args.d, f"{args.o}_{filecounter}.R2.fastq"), 'w')
+            out = open(os.path.join(args.d, f"{args.o}_{filecounter}_R2.fastq"), 'w')
             if args.v:
                 message(f"Writing right reads to {filecounter}", "GREEN")
-        if sid in reads and reads[sid] != filecounter:
+        tsid = sid.replace('/2', '')
+        if tsid in reads and reads[tsid] != filecounter:
             message("ERROR: Different file locations for {sid}. Left read is in {reads[sid]}. Right read is in {filecounter}. Please ensure your sequences are paired!", "RED")
-        if sid not in reads:
-            message("ERROR: Found an unpaire read, {sid}")
+        if tsid not in reads:
+            message(f"ERROR: Found an unpaired read, {tsid}", "PINK")
         out.write(f"@{seqid}\n{seq}\n+\n{qual}\n")
-        reads[sid] = filecounter
     out.close()
 
 
