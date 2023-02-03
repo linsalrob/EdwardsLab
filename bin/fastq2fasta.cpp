38dbd5083b656fde999ca878ec0368e1d932ff2b
linsalrob <raedwards@gmail.com>
2023-02-03 14:07:46 +1030

    adding the options to renumber sequences

--- bin/fastq2fasta.cpp
+++ bin/fastq2fasta.cpp
@@ -3,36 +3,96 @@
 #include <fstream>
 #include <string>
 #include <cstring>
+#include <getopt.h>
 using namespace std;
+
+void helpme() {
+  fprintf(stderr,
+      "fastq2fasta [options] <fastq file> <fasta file>\n"
+      "Options:\n"
+      "\t-r replace spaces in the fasta header line with '_'\n"
+      "\t-n <1/2> add the read number to the header line at the first space\n"
+   );
+}
 
 int main (int argc, char* argv[]) {
 
   if ( argc < 3) {
-    cerr << "Usage: " << argv[0] << " <fastq file (use - to read from STDIN)> <fasta file>\n";
+    helpme();
     return 1;
   }
 
+  int replace = 0;
+  string n;
+
+  for (;;) {
+    switch(getopt(argc, argv, "rn:")) {
+      default:
+        helpme();
+        return 1;
+      case -1:
+        break;
+      case 'r':
+        replace = 1;
+        continue;
+      case 'n':
+        n = optarg;
+        continue;
+    }
+    break;
+  }
+  
+  if (optind +2 != argc) {
+    helpme();
+    return 1;
+  }
+
+  char* fqf = argv[optind];
+  char* faf = argv[optind+1]; 
+
 
   ifstream fastq;
-  streambuf* orig_cin = 0;
-  if (strcmp(argv[1], "-") != 0) {
-    cout << "reading from " << argv[1] << '\n';
-    fastq.open(argv[1]);
-    if (!fastq) return 1;
-    orig_cin = cin.rdbuf(fastq.rdbuf());
-    cin.tie(0); // tied to cout by default
+  // streambuf* orig_cin = 0;
+  if (strcmp(fqf, "-") != 0) {
+	  cout << "reading from " << fqf << '\n';
+	  fastq.open(fqf);
+	  if (!fastq) return 1;
+	  // orig_cin = cin.rdbuf(fastq.rdbuf());
+	  cin.rdbuf(fastq.rdbuf());
+	  cin.tie(0); // tied to cout by default
   }
 
   string line;
-  ofstream fasta(argv[2]);
+  ofstream fasta(faf);
   if (!fasta) return 2;
   int c=0;
-  
+
   while (getline(cin, line))
   {
     //cout << c << " : " << line << '\n';
     if ( c==0 ) {
       line.replace(0, 1, ">");
+      if (replace) {
+	       for (long unsigned int i = 0; i <= line.length(); i++) {
+		       if (line[i] == ' ') {
+			       line[i] = '_';
+		       }
+	       }
+      }
+      else if (n.size()) {
+	      int changed = 0;
+	      for (long unsigned int i = 0; i <= line.length(); i++) {
+		      if (line[i] == ' ') {
+			      line.insert(i++, string("/") + n);
+			      changed = 1;
+			      break;
+		      }
+	      }
+	      if (changed == 0) {
+		      line += string("/") + n;
+	      }
+      }
+
       fasta << line << '\n';
     }
     if ( c==1 ) {
@@ -43,8 +103,8 @@ int main (int argc, char* argv[]) {
   }
 
   if ( c != 0 ) {
-	  cerr << "ERROR: There appears to be the wrong number of lines in your file!" << endl;
-	return 1;
+    cerr << "ERROR: There appears to be the wrong number of lines in your file!" << endl;
+  return 1;
   }
 
   return 0;
