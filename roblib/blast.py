ee812dab71d7fe0e67ab072de8359f68e66c67b6
linsalrob <raedwards@gmail.com>
2023-02-05 14:35:41 +1030

    adding  lengths

--- roblib/blast.py
+++ roblib/blast.py
@@ -22,6 +22,8 @@ class BlastResult():
         self.db_end = int(db_end)
         self.evalue = float(evalue)
         self.bitscore = float(bitscore)
+        self.query_length = None
+        self.subject_length = None
         if query_length:
             self.query_length = int(query_length)
         if subject_length:
@@ -36,9 +38,9 @@ class BlastResult():
         return self.evalue < 1e-5
 
     def __str__(self):
-        return "\t".join(map(str, self.query, self.db, self.alignment_length, self.percent_id, self.gaps,
+        return "\t".join(map(str, [self.query, self.db, self.alignment_length, self.percent_id, self.gaps,
                              self.mismatches, self.query_start, self.query_end, self.db_start, self.db_end,
-                             self.evalue, self.bitscore, self.query_length, self.subject_length))
+                             self.evalue, self.bitscore, self.query_length, self.subject_length]))
 
 def stream_blast_results(blastf, verbose=False):
     """
