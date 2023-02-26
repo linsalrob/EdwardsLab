8456bb0bedc13061d9efed04f082958f37db9335
linsalrob <raedwards@gmail.com>
2023-02-26 13:46:22 +1030

    using environmant variables for taxondb

--- taxon/config.py
+++ taxon/config.py
@@ -2,13 +2,25 @@
 Some settings for the config files
 """
 
-# defaultdir = '/data/ncbi/taxonomy/current'
-# defaultdir = '/home/edwa0468/ncbi/taxonomy'
-defaultdir = '/raid60/usr/data/NCBI/taxonomy/current/'
+import os
+import sys
+
 
 def get_db_dir():
     """
     Just return the default dir listed above
     :return: the default location for the sqllite database
     """
-    return defaultdir
+
+    if 'NCBI_TAXONOMY' in os.environ:
+        if os.path.exists(os.environ['NCBI_TAXONOMY']):
+            return os.environ['NCBI_TAXONOMY']
+        else:
+            print(f"WARNING: NCBI_TAXONOMY variable is set but {os.environ['NCBI_TAXONOMY']} does not exist", file=sys.stderr)
+    if 'TAXONKIT_DB' in os.environ:
+        if os.path.exists(os.environ['TAXONKIT_DB']):
+            return os.environ['TAXONKIT_DB']
+        else:
+            print(f"WARNING: TAXONKIT_DB variable is set but {os.environ['TAXONKIT_DB']} does not exist", file=sys.stderr)
+
+    return None
