477606b3d319746394ae701342cdca0ddc367fcf
linsalrob <raedwards@gmail.com>
2023-06-30 10:42:37 +0930

    testing some hashes

--- rob_tests/hashing.c
+++ rob_tests/hashing.c
@@ -10,9 +10,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <limits.h>
 
 // alpha for our hash function
-#define alpha 31
+#define alpha 5
 
 long double hash(char* seq) {
 	// calculate the hash function for a single string
@@ -27,6 +28,18 @@ long double hash(char* seq) {
 long double next_hash(char* seq, int p, int k, long double last_hash) {
 	
 	return (long double) alpha * (last_hash - (pow((long double) alpha, (long double) k) * (long double) seq[p-1]) + (long double) seq[p+k-1]);
+}
+
+unsigned long long ahash(char* seq) {
+	unsigned long long h = 0;
+	int c = 0;
+	while ((c=*seq++)) {
+		unsigned long long nh = alpha * (h + c);
+		if ((LLONG_MAX - h) < nh)
+			fprintf(stderr, "\033[91mWARNING: %lld will be exceeded!!\033[0m\n", LLONG_MAX);
+		h = alpha * (h + c);
+	}
+	return h;
 }
 
 
@@ -52,6 +65,9 @@ int main(int argc, char *argv[]) {
 	diff = hu - ht;
 	printf("Hash of %s is %Lf. Hash of %s is %Lf. Hash of substring is %Lf. Delta is %Lf\n", s, hs, t, ht, hu, diff);
 
+	unsigned long long hhs = ahash(s);
+	unsigned long long hht = ahash(t);
+	printf("aHash of %s is %lld. aHash of %s id %lld\n", s, hhs, t, hht);
 
 }
 	
