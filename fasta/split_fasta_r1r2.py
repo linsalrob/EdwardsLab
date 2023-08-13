f69aa7569aa13b11c4030955b18bf8ba1a58639b
linsalrob <raedwards@gmail.com>
2023-08-14 07:47:35 +0930

    split a fasta file into two reads

--- fasta/split_fasta_r1r2.py
+++ fasta/split_fasta_r1r2.py
@@ -17,12 +17,25 @@ if __name__ == "__main__":
     parser.add_argument('-v', help='verbose output', action='store_true')
     args = parser.parse_args()
 
+    sequential = True
+    lastid = None
     with open(args.read1, 'w') as r1out, open(args.read2, 'w') as r2out:
         for seqid, seq in stream_fasta(args.f, True):
             r = seqid.split(" ")[0][-1]
             if r == '1':
                 print(f">{seqid}\n{seq}", file=r1out)
+                if lastid:
+                    sequential = False
+                else:
+                    lastid = seqid.split(" ")[0][:-1]
             elif r == '2':
                 print(f">{seqid}\n{seq}", file=r2out)
+                if not lastid:
+                    sequential = False
+                else:
+                    if lastid != seqid.split(" ")[0][:-1]:
+                        sequential = False
             else:
                 print(f"Can't parse 1/2 from {seqid}", file=sys.stderr)
+    if not sequential:
+        print("WARNING: The sequences are NOT in sequential order. If that matters you should reorder them!")
